a15c5c5cedbe6ee58846109476ee40a781c2ef42
s.janjic <janjicstefan163@gmail.com>
2021-09-24 21:42:07 +0200

    Svelte - Pokemon API

---
+++ src/routes/about.svelte
@@ -0,0 +1,8 @@
+<svelte:head>
+  <title>This is About page</title>
+</svelte:head>
+<h1 class="text-center">About</h1>
+<p>
+  This is a really cool Svelte Kit demo 
+  that uses the Pokemon API to build a Pokedex
+</p>
---
+++ tailwind.config.cjs
@@ -0,0 +1,5 @@
+// tailwind.config.js
+module.exports = {
+	mode: 'jit',
+	purge: ['./src/**/*.svelte', './src/**/*.js']
+};
